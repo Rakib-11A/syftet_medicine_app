ae319df694a63f8850a93a28ec1405c948fa5f04
emrancub <myself.emranhasan@gmail.com>
2021-11-05 19:58:54 +0600

    Add Coupon

---
+++ db/migrate/20211105050919_create_admin_coupons.rb
@@ -0,0 +1,14 @@
+class CreateAdminCoupons < ActiveRecord::Migration[5.2]
+  def change
+    create_table :admin_coupons do |t|
+      t.string :code
+      t.string :name
+      t.integer :discount
+      t.integer :percentage
+      t.date :expiration
+      t.integer :maximun_limit
+
+      t.timestamps
+    end
+  end
+end
